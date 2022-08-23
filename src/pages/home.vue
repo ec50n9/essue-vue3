6ea0005c16342afd5092ef1563ff495ededfcd55
ec50n9 <shelloworld@qq.com>
2022-08-23 18:41:53 +0800

    完成home页面

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,5 +1,61 @@
 <script setup lang="ts">
 
+interface MixtureListItem {
+    type: 'wiki' | 'issue' | 'square',
+    title: string,
+    preview: string,
+    date: string,
+    tags: (string | { text: string, class: string })[],
+    voteCount: number,
+    commentCount: number
+}
+
+const dataList: MixtureListItem[] = [
+    {
+        type: 'wiki',
+        title: '如何评价全新的 ARC 浏览器，号称要重新定义网页？',
+        preview: '少数派： TL;DR <strong>优点（The good）</strong> 对标签过载问题提出了新颖的方案 交互设计细致入微，重引导而非强迫 网页修改功能有黑客精神和早期浏览器遗风 非常美貌（取决于你的偏好） 缺点（The bad）过于美貌（…',
+        date: '三天前',
+        tags: [
+            { text: '🔥 热门', class: 'tag--red' },
+            { text: '#日落', class: 'tag--green' },
+            '#篮球'
+        ],
+        voteCount: 76,
+        commentCount: 11
+    }, {
+        type: 'issue',
+        title: '如何评价全新的 ARC 浏览器，号称要重新定义网页？',
+        preview: '少数派： TL;DR <strong>优点（The good）</strong> 对标签过载问题提出了新颖的方案 交互设计细致入微，重引导而非强迫 网页修改功能有黑客精神和早期浏览器遗风 非常美貌（取决于你的偏好） 缺点（The bad）过于美貌（…',
+        date: '三天前',
+        tags: [
+            { text: '#日落', class: 'tag--green' }
+        ],
+        voteCount: 76,
+        commentCount: 11
+    }, {
+        type: 'square',
+        title: '如何评价全新的 ARC 浏览器，号称要重新定义网页？',
+        preview: '少数派： TL;DR <strong>优点（The good）</strong> 对标签过载问题提出了新颖的方案 交互设计细致入微，重引导而非强迫 网页修改功能有黑客精神和早期浏览器遗风 非常美貌（取决于你的偏好） 缺点（The bad）过于美貌（…',
+        date: '三天前',
+        tags: [
+            { text: '#广青', class: 'tag--green' },
+            '#篮球',
+            {text: '#烧烤', class:'tag--red'}
+        ],
+        voteCount: 76,
+        commentCount: 11
+    }
+]
+
+const type2Tag = (type: MixtureListItem['type']) => {
+    const obj = {
+        wiki: { class: 'tag--blue', text: '文章' },
+        issue: { class: 'tag--red', text: '问题' },
+        square: { class: 'tag--green', text: '广场' }
+    }
+    return obj[type]
+}
 </script>
 
 <template>
@@ -9,54 +65,26 @@
     </div>
     <div class="banner h-64 rounded-xl bg-blue-100"></div>
     <ul class="list mt-4">
-        <li>
+        <li v-for="(item, index) in dataList" :key="index">
             <div class="card py-4 flex flex-col gap-4">
                 <h3 class="text-xl"><span
-                        class="rounded-md px-2 py-1 bg-blue-100 text-sm text-blue-700 align-middle">文章</span>
-                    如何评价全新的 ARC 浏览器，号称要重新定义网页？</h3>
+                        class="tag tag--normal" :class="type2Tag(item.type).class||'tag--blue'">{{type2Tag(item.type).text}}</span>
+                    {{ item.title }}</h3>
                 <p class="text-gray-500 tracking-wide">
-                    少数派： TL;DR <strong>优点（The good）</strong> 对标签过载问题提出了新颖的方案 交互设计细致入微，重引导而非强迫
-                    网页修改功能有黑客精神和早期浏览器遗风 非常美貌（取决于你的偏好） <strong>缺点（The bad）</strong>过于美貌（…
+                    {{ item.preview }}
                 </p>
-                <div class="flex items-center gap-3 text-gray-500 text-sm">
-                    <div class="flex-grow">三天前</div>
-                    <div class="px-2 py-1 rounded-md bg-red-100 text-red-500 font-medium">🔥 热门</div>
-                    <div class="px-2 py-1 rounded-md bg-blue-100 text-blue-500 font-medium">76 点赞</div>
-                    <div class="px-2 py-1 rounded-md bg-yellow-100 text-yellow-500 font-medium">11 评论</div>
-                </div>
-            </div>
-        </li>
-        <li>
-            <div class="card py-4 flex flex-col gap-4">
-                <h3 class="text-xl"><span
-                        class="rounded-md px-2 py-1 bg-red-100 text-sm text-red-700 align-middle">问题</span>
-                    如何评价全新的 ARC 浏览器，号称要重新定义网页？</h3>
-                <p class="text-gray-500 tracking-wide">
-                    少数派： TL;DR <strong>优点（The good）</strong> 对标签过载问题提出了新颖的方案 交互设计细致入微，重引导而非强迫
-                    网页修改功能有黑客精神和早期浏览器遗风 非常美貌（取决于你的偏好） <strong>缺点（The bad）</strong>过于美貌（…
-                </p>
-                <div class="flex items-center gap-3 text-gray-500 text-sm">
-                    <div class="flex-grow">两个小时前</div>
-                    <div class="px-2 py-1 rounded-md bg-blue-100 text-blue-500 font-medium">76 点赞</div>
-                    <div class="px-2 py-1 rounded-md bg-yellow-100 text-yellow-500 font-medium">11 评论</div>
-                </div>
-            </div>
-        </li>
-        <li>
-            <div class="card py-4 flex flex-col gap-4">
-                <h3 class="text-xl"><span
-                        class="rounded-md px-2 py-1 bg-green-100 text-sm text-green-700 align-middle">广场</span>
-                    二饭有没有什么好吃的店</h3>
-                <p class="text-gray-500 tracking-wide">
-                    少数派： TL;DR <strong>优点（The good）</strong> 对标签过载问题提出了新颖的方案 交互设计细致入微，重引导而非强迫
-                    网页修改功能有黑客精神和早期浏览器遗风 非常美貌（取决于你的偏好） <strong>缺点（The bad）</strong>过于美貌（…
-                </p>
-                <div class="flex items-center gap-3 text-gray-500 text-sm">
-                    <div class="flex-grow">片刻之前</div>
-                    <div class="px-2 py-1 rounded-md bg-blue-100 text-blue-500 font-medium">76 点赞</div>
-                    <div class="px-2 py-1 rounded-md bg-yellow-100 text-yellow-500 font-medium">11 评论</div>
+                <div class="flex items-center gap-3 text-gray-500">
+                    <div class="tag tag--gray">{{ item.date }}</div>
+                    <div class="flex-grow"></div>
+                    <div v-for="tag in item.tags" class="tag tag--red" :class="tag.class || 'tag--blue'">
+                        {{ tag.text || tag }}</div>
+                    <div class="tag tag--blue">👍 {{ item.voteCount }} 点赞</div>
+                    <div class="tag tag--yellow">💬 {{ item.commentCount }} 评论</div>
                 </div>
             </div>
         </li>
     </ul>
 </template>
+
+<style scoped>
+</style>
