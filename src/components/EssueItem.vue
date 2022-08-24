7613e08c98a6925ca3eb034e6c65b68dff9d7f72
ec50n9 <shelloworld@qq.com>
2022-08-24 09:39:59 +0800

    根据传入图片的数量来布局，超过一张图片就隐藏文字，只有一张图片就和文字一同显示，没有图片就只显示文字

--- src/components/EssueItem.vue
+++ src/components/EssueItem.vue
@@ -3,16 +3,23 @@ const props = defineProps<{ data: MixtureListItem }>()
 </script>
 
 <template>
-  <div class="card py-4 flex flex-col gap-2">
+  <div class="card py-4 flex flex-col gap-4">
     <h3 class="text-xl"><span class="tag tag--normal"
                               :class="data.type.class||'tag--blue'">{{ data.type.text || data.type }}</span>
       {{ data.title }}</h3>
-    <p v-if="data.preview" class="text-gray-500 tracking-wide">
-      <span v-html="data.preview"></span>
-    </p>
+    <!--封面和预览内容-->
+    <div class="flex gap-4">
+      <div v-if="data.covers" class="flex-shrink-0 flex-grow flex justify-between gap-4">
+        <img v-for="(cover, index) in data.covers" :key="index" :src="cover" :alt="cover"
+             class="flex-grow h-32 object-cover rounded-xl shadow shadow-blue-100">
+      </div>
+      <p v-if="data.preview&&(!data.covers||data.covers.length<=1)" class="flex-grow text-gray-500 tracking-wide">
+        <span v-html="data.preview"></span>
+      </p>
+    </div>
     <div class="flex items-center gap-3 text-gray-500">
       <div class="tag tag--gray">{{ data.date }}</div>
-      <div v-if="data.author" class="w-fit tag tag--gray text-gray-500">@{{data.author}}</div>
+      <div v-if="data.author" class="w-fit tag tag--gray text-gray-500">@{{ data.author }}</div>
       <div class="flex-grow"></div>
       <div v-for="tag in data.tags" class="tag tag--red" :class="tag.class || 'tag--blue'">
         {{ tag.text || tag }}
