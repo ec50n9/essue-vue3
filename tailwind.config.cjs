831703c84bb509556684fe334219b622febf26ed
ec50n9 <shelloworld@qq.com>
2022-08-24 14:48:53 +0800

    引入flowbite

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    // Example content paths...
-    './public/**/*.html',
-    './src/**/*.{js,jsx,ts,tsx,vue}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        // Example content paths...
+        './public/**/*.html',
+        './src/**/*.{js,jsx,ts,tsx,vue}',
+        './node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx}',
+        './node_modules/flowbite/**/*.{js,jsx,ts,tsx}'
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require('flowbite/plugin')
+    ],
 };
