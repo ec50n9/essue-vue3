7741498ca3384547c1de96432cc37226aba6017a
ec50n9 <shelloworld@qq.com>
2022-08-24 14:49:06 +0800

    完成登录页面

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,10 +1,31 @@
 <script setup lang="ts">
-import EcFloatingLabel from "../components/EcFloatingLabel.vue";
+import FloatingLabel from "../components/FloatingLabel.vue";
+import {Button, Toast, Tooltip} from 'flowbite-vue';
+import {ref} from "vue";
+
+const loginForm = ref({
+  username: '',
+  password: ''
+})
+
+const loginHandler = ()=>{
+  console.log(loginForm.value)
+}
 </script>
 
 <template>
-  <div class="w-96 p-8">
-    <EcFloatingLabel type="filled" place-holder="请输入账号"></EcFloatingLabel>
+  <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-96 p-8 flex flex-col gap-y-6">
+    <div class="">
+      <h1 class="mb-2 text-3xl text-gray-700">别来无恙！</h1>
+      <p class="text-gray-500">快登录，拿回属于你的一切～</p>
+    </div>
+    <FloatingLabel name="username" type="outlined" v-model="loginForm.username" place-holder="账号"/>
+    <FloatingLabel name="password" type="outlined" v-model="loginForm.password" place-holder="密码" cover/>
+    <div>
+      <Button class="w-full" @click="loginHandler">登录</Button>
+      <p class="my-2 text-center text-gray-500 text-sm">没有账号？你可以👇</p>
+      <Button class="w-full" outline>注册</Button>
+    </div>
   </div>
 </template>
 
--- src/pages/main.vue
+++ src/pages/main.vue
@@ -2,92 +2,96 @@
 import router from '../router'
 
 const navList = [
-    {
-        path:'/',
-        name: '首页'
-    },{
-        path: '/issues',
-        name: '问答'
-    },{
-        path: '/wiki',
-        name: '知识库'
-    },{
-        path: '/square',
-        name: '广场'
-    },{
-        path: '/about',
-        name: '关于我们'
-    }
+  {
+    path: '/',
+    name: '首页'
+  }, {
+    path: '/issues',
+    name: '问答'
+  }, {
+    path: '/wiki',
+    name: '知识库'
+  }, {
+    path: '/square',
+    name: '广场'
+  }, {
+    path: '/about',
+    name: '关于我们'
+  }
 ]
 </script>
 
 <template>
-    <header class="sticky top-0 w-full border-b-2 bg-white">
-        <!-- 顶部栏 -->
-        <div class="container mx-auto flex h-16 items-center gap-4">
-            <h1 class="text-xl font-medium">#Essue</h1>
-            <div class="flex-grow"></div>
-            <div class="search-bar">
-                <form>
-                    <label for="default-search"
-                        class="sr-only mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Search</label>
-                    <div class="relative">
-                        <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                            <svg aria-hidden="true" class="h-5 w-5 text-gray-500 dark:text-gray-400" fill="none"
-                                stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                            </svg>
-                        </div>
-                        <input type="search" id="default-search"
-                            class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2 pl-10 text-sm text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-                            placeholder="搜索问题..." required />
-                    </div>
-                </form>
+  <header class="sticky top-0 w-full border-b-2 bg-white">
+    <!-- 顶部栏 -->
+    <div class="container mx-auto flex h-16 items-center gap-4">
+      <h1 class="text-xl font-medium">#Essue</h1>
+      <div class="flex-grow"></div>
+      <div class="search-bar">
+        <form>
+          <label for="default-search"
+                 class="sr-only mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Search</label>
+          <div class="relative">
+            <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+              <svg aria-hidden="true" class="h-5 w-5 text-gray-500 dark:text-gray-400" fill="none"
+                   stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+              </svg>
             </div>
-            <!-- 头像 -->
-            <div class="avatar h-11 w-11 rounded-full bg-blue-100"></div>
-        </div>
-    </header>
-    <div class="wrapper container mx-auto flex gap-4">
-        <!-- 侧边栏 -->
-        <div class="side-bar flex-shrink-0 w-3/12 bg-white">
-            <div class="sticky top-16 pt-8">
-                <!-- 个人信息卡片 -->
-                <div class="mb-8">
-                    <div class="rounded-lg bg-blue-100 p-4">
-                        <h3 class="text-gray-500">今日活力：</h3>
-                        <p class="mt-2 text-center text-xl font-bold text-gray-500">999</p>
-                    </div>
-                </div>
-                <!-- 菜单列表 -->
-                <ul class="nav">
-                    <RouterLink v-for="item in navList" :key="item.path" :to="item.path">
-                        <li class="nav-item" :class="{'nav-item--active': router.options.history.location===item.path}">{{item.name}}</li>
-                    </RouterLink>
-                </ul>
-            </div>
-        </div>
-        <div class="content flex-grow w-max p-8">
-            <RouterView></RouterView>
-        </div>
+            <input type="search" id="default-search"
+                   class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2 pl-10 text-sm text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                   placeholder="搜索问题..." required/>
+          </div>
+        </form>
+      </div>
+      <!-- 头像 -->
+      <RouterLink to="/login">
+        <div class="avatar h-11 w-11 rounded-full bg-blue-100"></div>
+      </RouterLink>
     </div>
+  </header>
+  <div class="wrapper container mx-auto flex gap-4">
+    <!-- 侧边栏 -->
+    <div class="side-bar flex-shrink-0 w-3/12 bg-white">
+      <div class="sticky top-16 pt-8">
+        <!-- 个人信息卡片 -->
+        <div class="mb-8">
+          <div class="rounded-lg bg-blue-100 p-4">
+            <h3 class="text-gray-500">今日活力：</h3>
+            <p class="mt-2 text-center text-xl font-bold text-gray-500">999</p>
+          </div>
+        </div>
+        <!-- 菜单列表 -->
+        <ul class="nav">
+          <RouterLink v-for="item in navList" :key="item.path" :to="item.path">
+            <li class="nav-item" :class="{'nav-item--active': router.options.history.location===item.path}">
+              {{ item.name }}
+            </li>
+          </RouterLink>
+        </ul>
+      </div>
+    </div>
+    <div class="content flex-grow w-max p-8">
+      <RouterView></RouterView>
+    </div>
+  </div>
 </template>
 
 <style scoped>
 .nav {
-    @apply flex flex-col space-y-4;
+  @apply flex flex-col space-y-4;
 }
 
 .nav-item {
-    @apply px-5 py-2 rounded-lg cursor-pointer duration-300 text-gray-500;
+  @apply px-5 py-2 rounded-lg cursor-pointer duration-300 text-gray-500;
 }
 
 .nav-item:hover {
-    @apply bg-indigo-100;
+  @apply bg-indigo-100;
 }
 
 .nav-item--active {
-    @apply bg-indigo-100 text-blue-700;
+  @apply bg-indigo-100 text-blue-700;
 }
 </style>
