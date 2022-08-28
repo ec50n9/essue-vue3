71c6e3339c22d6174fe75a87bddfdc5833b8b844
ec50n9 <shelloworld@qq.com>
2022-08-28 16:48:08 +0800

    微调toast样式

--- src/components/toast/EcToast.vue
+++ src/components/toast/EcToast.vue
@@ -37,7 +37,7 @@ const visible = ref(true)
         <span class="sr-only">Warning icon</span>
       </div>
     </template>
-    <div class="ml-3 text-sm font-normal">{{ message }}</div>
+    <div class="mx-4 text-sm font-normal">{{ message }}</div>
     <button @click="visible = false" type="button"
             class="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
             data-dismiss-target="#toast-default" aria-label="Close">
