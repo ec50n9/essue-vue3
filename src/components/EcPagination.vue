6fdbd9c89ebccc4675d1c3a21ddea12d6818fa05
ec50n9 <shelloworld@qq.com>
2022-09-05 16:37:26 +0800

    分页组件在第一页和最后一页时禁用上一页和下一页

--- src/components/EcPagination.vue
+++ src/components/EcPagination.vue
@@ -40,15 +40,15 @@ const jumpPageHandler = (pageNum: number) => {
   <nav class="py-8 text-center" aria-label="页面导航">
     <ul class="inline-flex -space-x-px">
       <li>
-        <a href="#" class="pagination-item rounded-l-lg" @click="previousPageHandler">上一页</a>
+        <span class="pagination-item rounded-l-lg" :class="pagination.current===1?'pagination-item--disable':''" @click="previousPageHandler">上一页</span>
       </li>
       <li v-for="i in [...Array(Math.ceil(pagination.total/pagination.size)).keys()]">
-        <a href="#" class="pagination-item"
+        <span class="pagination-item"
            :class="pagination.current===i+1?'pagination-item--active':''"
-           @click="jumpPageHandler(i+1)">{{ i + 1 }}</a>
+              @click="jumpPageHandler(i+1)">{{ i + 1 }}</span>
       </li>
       <li>
-        <a href="#" class="pagination-item rounded-r-lg" @click="nextPageHandler">下一页</a>
+        <span class="pagination-item rounded-r-lg" :class="Math.ceil(pagination.total / pagination.size)===pagination.current?'pagination-item--disable':''" @click="nextPageHandler">下一页</span>
       </li>
     </ul>
   </nav>
@@ -57,10 +57,15 @@ const jumpPageHandler = (pageNum: number) => {
 
 <style scoped>
 .pagination-item {
-  @apply py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white;
+  @apply py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white
+        cursor-pointer select-none;
 }
 
 .pagination-item--active {
   @apply py-2 px-3 text-blue-600 bg-blue-50 border border-gray-300 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white;
+}
+
+.pagination-item--disable{
+  @apply text-gray-300 hover:text-gray-300 hover:bg-white cursor-not-allowed;
 }
 </style>
